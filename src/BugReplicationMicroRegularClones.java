3f8574d1e25653c35f72a74e59d527d2f76c7278
Judith <judithfran@gmail.com>
2020-06-12 22:46:14 -0600

    Implementation of RQ2 is completed for both regular and micro-clones.

--- src/BugReplicationMicroRegularClones.java
+++ src/BugReplicationMicroRegularClones.java
@@ -449,6 +449,169 @@ public class BugReplicationMicroRegularClones {
         }
     }
 
+    //-------------------------------------- This function implementing RQ2 ---------------------------------------
+
+    public void bugReplicationRQ2(){
+        try{
+
+            // --------------------------Implementing RQ2 for Regular Clones----------------------------
+            ArrayList<CodeFragment> bugRepR = new ArrayList<>();
+
+            bugRepR = bugReplicationR();
+
+            int classID1R = 0, classID2R = 0, countRepR = 1, nclonesR = 0, flagR = 0;
+
+            for(int i = 0; i < bugRepR.size(); i++){
+                for(int j = i+1; j < bugRepR.size(); j++){
+                    //if(bugRepR.get(i).revision == bugRepR.get(j).revision && bugRepR.get(i).filepath.equals(bugRepR.get(j).filepath)){
+                    if(bugRepR.get(i).revision == bugRepR.get(j).revision){
+                        System.out.println("Revision number (R) = " + bugRepR.get(i).revision);
+                        classID1R = getClassID(bugRepR.get(i));
+                        classID2R = getClassID(bugRepR.get(j));
+
+                        if(classID1R == classID2R){
+                            countRepR++;
+                            i++;
+                        }
+                        flagR = 1;
+
+                    }
+
+                }
+                if(flagR == 1){
+                    nclonesR = nclonesR + getCloneNumber(bugRepR.get(i-1));
+                    flagR = 0;
+                }
+
+                System.out.println("At i = " + i + " countRepR = " + countRepR);
+                System.out.println("At i = " + i + " nclonesR = " + nclonesR);
+
+                PrintWriter writer = new PrintWriter("Temp_output.txt", "UTF-8");
+
+                writer.println("At i = " + i + " countRepR = " + countRepR);
+                writer.println("At i = " + i + " nclonesR = " + nclonesR);
+
+                writer.close();
+
+                countRepR++;
+            }
+
+            // --------------------------Implementing RQ2 for Micro Clones----------------------------
+            ArrayList<CodeFragment> bugRepM = new ArrayList<>();
+
+            bugRepM = bugReplicationM();
+
+            int classID1M = 0, classID2M = 0, countRepM = 1, nclonesM = 0, flagM = 0;
+
+            for(int i = 0; i < bugRepM.size(); i++){
+                for(int j = i+1; j < bugRepM.size(); j++){
+                    //if(bugRepR.get(i).revision == bugRepR.get(j).revision && bugRepR.get(i).filepath.equals(bugRepR.get(j).filepath)){
+                    if(bugRepM.get(i).revision == bugRepM.get(j).revision){
+                        System.out.println("Revision number (M) = " + bugRepM.get(i).revision);
+                        classID1M = getClassID(bugRepM.get(i));
+                        classID2M = getClassID(bugRepM.get(j));
+
+                        if(classID1M == classID2M){
+                            countRepM++;
+                            i++;
+                        }
+                        flagM = 1;
+
+                    }
+
+                }
+                if(flagM == 1){
+                    nclonesM = nclonesM + getCloneNumberMicro(bugRepM.get(i-1));
+                    flagM = 0;
+                }
+
+                System.out.println("At i = " + i + " countRepM = " + countRepM);
+                System.out.println("At i = " + i + " nclonesM = " + nclonesM);
+                countRepM++;
+            }
+        }catch(Exception e){
+            System.out.println("Error in bugReplicationRQ2: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public int getCloneNumber(CodeFragment cf) {
+        // In this method I use cfFile (a two dimensional array) to store each clone file (ConQat clone output file). In first dimension it will store the class number (classID)
+        // and in second dimension it will store each clone fragments.
+        CodeFragment[][] cfFile = new CodeFragment[10000][10000];
+        int numClones = 0;
+        try{
+            File file = new File(InputParameters.pathClone + cf.revision + "/clones-gapped.xml"); //All Type
+
+            if(file.exists()) {
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file))); // All Type
+
+                while((br.readLine()) != null) {
+                    cfFile = fileRead(cf.revision);
+
+                    int flag = 0;
+                    for (int i = 0; i < cfFile.length; i++) {
+                        for (int j = 0; j < cfFile.length; j++) {
+                            if (cfFile[i][j] != null) {
+                                if (cf.filepath.equals(cfFile[i][j].filepath) && cf.startline == cfFile[i][j].startline && cf.endline == cfFile[i][j].endline) {
+                                    flag = 1;
+                                }
+                                numClones = j;
+                                //System.out.println("The value of variable numClones inside the inner for loop = " + numClones);
+                            }
+                        }
+                        if (flag == 1) {
+                            //System.out.println("The value of variable numClones returned from getCloneNumber = " + numClones);
+                            return numClones+1;
+                        }
+                    }
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error in getCloneNumber: " + e);
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getCloneNumberMicro(CodeFragment cf) {
+        // In this method I use cfFile (a two dimensional array) to store each clone file (ConQat clone output file). In first dimension it will store the class number (classID)
+        // and in second dimension it will store each clone fragments.
+        CodeFragment[][] cfFile = new CodeFragment[10000][10000];
+        int numClones = 0;
+        try{
+            File file = new File(InputParameters.pathClone + cf.revision + "/clones-gapped.xml"); //All Type
+
+            if(file.exists()) {
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file))); // All Type
+
+                while((br.readLine()) != null) {
+                    cfFile = fileReadMicro(cf.revision);
+
+                    int flag = 0;
+                    for (int i = 0; i < cfFile.length; i++) {
+                        for (int j = 0; j < cfFile.length; j++) {
+                            if (cfFile[i][j] != null) {
+                                if (cf.filepath.equals(cfFile[i][j].filepath) && cf.startline == cfFile[i][j].startline && cf.endline == cfFile[i][j].endline) {
+                                    flag = 1;
+                                }
+                                numClones = j;
+                            }
+                        }
+                        if (flag == 1)
+                            return numClones+1;
+                    }
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error in getCloneNumberMicro: " + e);
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
     public ArrayList<CodeFragment> bugReplicationR(){
         ArrayList<CodeFragment> bugRep = new ArrayList<>();
         try{
@@ -994,10 +1157,10 @@ public class BugReplicationMicroRegularClones {
     }
 
     public CodeFragment[][] fileRead(int rev){
-        CodeFragment[][] cfFile2 = new CodeFragment[1000][1000];
+        CodeFragment[][] cfFile2 = new CodeFragment[5000][5000];
         try {
-            CodeFragment[][] cfFile = new CodeFragment[1000][1000];
-            CodeFragment[][] cfFile1 = new CodeFragment[1000][1000];
+            CodeFragment[][] cfFile = new CodeFragment[5000][5000];
+            CodeFragment[][] cfFile1 = new CodeFragment[5000][5000];
 
             File fileConQat = new File(InputParameters.pathClone + rev + "/clones-gapped.xml"); //All Type
 
